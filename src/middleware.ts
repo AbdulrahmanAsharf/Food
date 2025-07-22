0908e392288d8692391e03773d4e79f087c67201
abdo <bodeasharf52@gmail.com>
2025-07-22 20:04:58 +0300

    fix: allow public images to be accessed without auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,6 +21,7 @@ export default clerkMiddleware(async (authFn, req) => {
 });
 
 export const config = {
- matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$|.*\\.webp$).*)",
+  ],
 };
